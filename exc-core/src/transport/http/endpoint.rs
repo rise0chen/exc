9f912c45350e10362f4f00d48b30d39a32e0d038
rise0chen <rise0chen@163.com>
2024-10-04 21:44:41 +0800

    a

--- exc-core/src/transport/http/endpoint.rs
+++ exc-core/src/transport/http/endpoint.rs
@@ -16,10 +16,9 @@ mod https {
                 if #[cfg(feature = "native-tls")] {
                     let https = hyper_tls::HttpsConnector::new();
                 } else if #[cfg(feature = "rustls-tls")] {
-                    use hyper_rustls::ConfigBuilderExt;
 
                     let https = hyper_rustls::HttpsConnectorBuilder::new();
-                    let https = https.with_tls_config(rustls::ClientConfig::builder_with_protocol_versions(&[&rustls::version::TLS13]).with_webpki_roots().with_no_client_auth());
+                    let https = https.with_webpki_roots();
                     let https = https.https_or_http();
                     #[cfg(not(feature = "http2"))]
                     let https = https.enable_http1();
